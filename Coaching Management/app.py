ba4e1fd2b7b7d5d70f29f74de5cb84c00210bc28
psyduck3035 <psyduck3035@gmail.com>
2026-02-05 00:25:50 +0530

    Update app.py

--- Coaching Management/app.py
+++ Coaching Management/app.py
@@ -6,13 +6,6 @@ from datetime import datetime
 from functools import wraps
 
 import os
-import psycopg2
-
-conn = psycopg2.connect(
-    os.getenv("DATABASE_URL"),
-    sslmode="require"
-)
-
 
 app = Flask(__name__)
 app.secret_key = os.environ.get("SECRET_KEY", "DEV_SECRET_CHANGE_ME")
@@ -1181,6 +1174,7 @@ def timetable():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
 
 
 
